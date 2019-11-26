87e1a67710e8616e41d7831a40761c7428257f51
Metje Ruberg <metje.ruberg@telekom.de>
2019-11-26 11:24:02 +0100

    Version 1.1

--- Nature.java
+++ Nature.java
@@ -3,6 +3,16 @@ package com.wilders.ICanFly;
 public class Nature {
 
 	public static void main(String[] args) {
+
+		Penguin pingou = new Penguin("Pingou");
+		pingou.dive();
+		pingou.swimDown(3);
+		pingou.swimUp(1);
+		pingou.swimDown(4);
+		pingou.swimUp(5);
+		pingou.swimUp(1);
+		pingou.getOut();
+
 		Eagle myEagle = new Eagle("Hawkey");
 
 		myEagle.takeOff();
@@ -13,8 +23,7 @@ public class Nature {
 		myEagle.land();
 		myEagle.decent(9);
 		myEagle.land();
-		
-		
+
 	}
 
 }
